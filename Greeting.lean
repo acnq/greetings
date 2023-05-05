ea1f5d633d383d9e9d37467550a461a584b77430
acnq <fanyuanhaotc@163.com>
2023-05-05 04:08:50 +0300

    test

--- Greeting.lean
+++ Greeting.lean
@@ -1,3 +1,18 @@
+-- Lake init
+-- Lake for Lean Make
+-- lean use do as special-purpose language
+-- as known as domain-specific embedded languages
+-- DSEL
+-- Lean use DSEL for configuring
+
+-- Lake init initialize 4 files
+-- Main.lean : which lean compiler look for main
+-- Greeting.lean : scaffolding of support lib
+-- lakefile.lean: configuration that lake need to build app
+-- lean-toolchain : id for lean version ?? not seen?
+-- .git/ and .gitignore
+
+
 def hello := "world"
 
 -- new change
