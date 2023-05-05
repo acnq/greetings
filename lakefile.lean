019832be9584c5a62a319d5e1eeaba45955b8319
acnq <fanyuanhaotc@163.com>
2023-05-05 05:17:18 +0300

    comments

--- lakefile.lean
+++ lakefile.lean
@@ -1,14 +1,19 @@
+-- describe package
+-- like npm or nuget package or rust package
 import Lake
 open Lake DSL
 
+-- package declaration, only 1
 package «greeting» {
   -- add package configuration options here
 }
 
+-- library declaration, 1+
 lean_lib «Greeting» {
   -- add library configuration options here
 }
 
+-- executable, 1+
 @[default_target]
 lean_exe «greeting» {
   root := `Main
